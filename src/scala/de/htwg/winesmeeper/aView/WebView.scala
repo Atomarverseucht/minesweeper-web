eb77698294476b73439f57f231e7eb72cabd8387
Atom <atomcraft.4u@web.de>
2026-02-14 02:07:17 +0100

    added WebView.scala

---
+++ src/scala/de/htwg/winesmeeper/aView/WebView.scala
@@ -0,0 +1,10 @@
+package de.htwg.winesmeeper.aView
+
+import de.htwg.winesmeeper.Controller.ControllerTrait
+import de.htwg.winesmeeper.Observer
+
+class WebView(ctrl: ControllerTrait) extends Observer(ctrl):
+  override def generate(): Unit = {}
+
+  override def update(): Unit = {}
+
