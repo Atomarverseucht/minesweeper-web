cb26fc1b6f61ab726b53c435139657ab5232fbae
Atomarverseucht <atomcraft.4u@web.de>
2026-03-24 21:33:16 +0100

    feat: new title

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -1,15 +1,15 @@
 import "./styles.css";
-import { Component } from "react";
+import {Component} from "react";
 import { createRoot } from "react-dom/client";
 import GameUI from "./components/GameUI";
+import {version} from "../../package.json";
 
 class App extends Component {
   public render() {
     return (
       <main>
-        <title>Minesweeper-Web</title>
         <h1>
-          💣 <a href="https://github.com/Atomarverseucht/minesweeper-web">Minesweeper Web</a>
+          💣 <a href="https://github.com/Atomarverseucht/minesweeper-web">Minesweeper Web (version {version})</a>
         </h1>
         <p>
           The web version of <a href="https://github.com/Atomarverseucht/minesweeper">Atoms and Guakocius Minesweeper project</a>.
@@ -19,5 +19,5 @@ class App extends Component {
     );
   }
 }
-
+document.title = `Minesweeper Web v${version}`;
 createRoot(document.getElementById("app")!).render(<App />);
